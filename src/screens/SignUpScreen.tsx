e36f3094d2e3158b3c3a8adb750e826499dc4738
Felipe Hoyos Taborda <felipe02-98@hotmail.com>
2023-10-03 16:12:02 -0500

    adding safe area view into signup screen

--- src/screens/SignUpScreen.tsx
+++ src/screens/SignUpScreen.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, TextInput, StyleSheet, TouchableOpacity } from 'react-native';
+import { View, Text, TextInput, StyleSheet, TouchableOpacity, SafeAreaView } from 'react-native';
 import CheqbookLogoHeader from '../components/CheqbookLogoHeader';
 import CustomButton from '../components/CustomButton';
 import { useButtonStates } from '../hooks/useButtonStates ';
@@ -15,98 +15,100 @@ export const SignUpScreen = ({ navigation, route }: Props) => {
   const { buttonStates, handleButtonPressIn, handleButtonPressOut } = useButtonStates();
 
   return (
-    <ScrollView>
-      <View style={styles.container}>
-        <View style={{
-          flex: 1,
-          marginVertical: '2%'
-        }}>
-          <CheqbookLogoHeader logoWidthPercentage={70} />
-        </View>
-        <View style={styles.inputContainer}>
-          <Text style={styles.inputLabel}>Name</Text>
-          <TextInput
-            style={styles.input}
-            selectionColor="gray"
+    <SafeAreaView>
+      <ScrollView>
+        <View style={styles.container}>
+          <View style={{
+            flex: 1,
+            marginVertical: '2%'
+          }}>
+            <CheqbookLogoHeader logoWidthPercentage={70} />
+          </View>
+          <View style={styles.inputContainer}>
+            <Text style={styles.inputLabel}>Name</Text>
+            <TextInput
+              style={styles.input}
+              selectionColor="gray"
+            />
+            <Text style={styles.inputLabel}>Email</Text>
+            <TextInput
+              style={styles.input}
+              secureTextEntry={true}
+              selectionColor="gray"
+            />
+          </View>
+
+          <CustomButton
+            buttonState={buttonStates.login}
+            handlePressIn={() => handleButtonPressIn('login')}
+            handlePressOut={() => handleButtonPressOut('login')}
+            buttonText="Sign up"
           />
-          <Text style={styles.inputLabel}>Email</Text>
-          <TextInput
-            style={styles.input}
-            secureTextEntry={true}
-            selectionColor="gray"
-          />
-        </View>
 
-        <CustomButton
-          buttonState={buttonStates.login}
-          handlePressIn={() => handleButtonPressIn('login')}
-          handlePressOut={() => handleButtonPressOut('login')}
-          buttonText="Sign up"
-        />
-
-        <View style={{
-          width: '80%', marginTop: hp('1.5%')
-        }}>
-          <CheckBox navigation={navigation} route={route} />
-        </View>
-
-        <View style={styles.separatorContainer}>
-          <View style={styles.separatorLine} />
-          <Text style={styles.separatorText}>OR</Text>
-          <View style={styles.separatorLine} />
-        </View>
-
-        <View style={styles.buttonContainer}>
-          <View>
-            <CustomButton
-              buttonState={buttonStates.loginWithGoogle}
-              buttonText='Sign up with Google'
-              handlePressIn={() => handleButtonPressIn('loginWithGoogle')}
-              handlePressOut={() => handleButtonPressOut('loginWithGoogle')}
-              imageSource={require('../../assets/google-logo.png')}
-            />
+          <View style={{
+            width: '80%', marginTop: hp('1.5%')
+          }}>
+            <CheckBox navigation={navigation} route={route} />
           </View>
 
-          <View>
-            <CustomButton
-              buttonState={buttonStates.loginWithFacebook}
-              handlePressIn={() => handleButtonPressIn('loginWithFacebook')}
-              handlePressOut={() => handleButtonPressOut('loginWithFacebook')}
-              buttonText="Sign up with Facebook"
-              imageSource={require('../../assets/facebook-logo.png')}
-            />
+          <View style={styles.separatorContainer}>
+            <View style={styles.separatorLine} />
+            <Text style={styles.separatorText}>OR</Text>
+            <View style={styles.separatorLine} />
           </View>
 
-          <View>
-            <CustomButton
-              buttonState={buttonStates.loginWithLinkedIn}
-              handlePressIn={() => handleButtonPressIn('loginWithLinkedIn')}
-              handlePressOut={() => handleButtonPressOut('loginWithLinkedIn')}
-              buttonText="Sign up with LinkedIn"
-              imageSource={require('../../assets/linkedin-logo.png')}
-            />
+          <View style={styles.buttonContainer}>
+            <View>
+              <CustomButton
+                buttonState={buttonStates.loginWithGoogle}
+                buttonText='Sign up with Google'
+                handlePressIn={() => handleButtonPressIn('loginWithGoogle')}
+                handlePressOut={() => handleButtonPressOut('loginWithGoogle')}
+                imageSource={require('../../assets/google-logo.png')}
+              />
+            </View>
+
+            <View>
+              <CustomButton
+                buttonState={buttonStates.loginWithFacebook}
+                handlePressIn={() => handleButtonPressIn('loginWithFacebook')}
+                handlePressOut={() => handleButtonPressOut('loginWithFacebook')}
+                buttonText="Sign up with Facebook"
+                imageSource={require('../../assets/facebook-logo.png')}
+              />
+            </View>
+
+            <View>
+              <CustomButton
+                buttonState={buttonStates.loginWithLinkedIn}
+                handlePressIn={() => handleButtonPressIn('loginWithLinkedIn')}
+                handlePressOut={() => handleButtonPressOut('loginWithLinkedIn')}
+                buttonText="Sign up with LinkedIn"
+                imageSource={require('../../assets/linkedin-logo.png')}
+              />
+            </View>
           </View>
-        </View>
 
-        <View style={styles.separatorContainer}>
-          <View style={styles.separatorLine} />
-        </View>
+          <View style={styles.separatorContainer}>
+            <View style={styles.separatorLine} />
+          </View>
 
-        <View style={styles.loginContainer}>
-          <Text style={styles.loginText}>Already have an account?</Text>
-          <TouchableOpacity
-            onPress={() => navigation.navigate('LoginScreen')}
-          >
-            <Text style={styles.signUpButton}>Log in</Text>
-          </TouchableOpacity>
-        </View>
+          <View style={styles.loginContainer}>
+            <Text style={styles.loginText}>Already have an account?</Text>
+            <TouchableOpacity
+              onPress={() => navigation.navigate('LoginScreen')}
+            >
+              <Text style={styles.signUpButton}>Log in</Text>
+            </TouchableOpacity>
+          </View>
 
-        <View style={styles.footerContainer}>
-          <Text style={styles.footerText}>We guarantee 100% privacy. Your financial information will not be shared.</Text>
-        </View>
+          <View style={styles.footerContainer}>
+            <Text style={styles.footerText}>We guarantee 100% privacy. Your financial information will not be shared.</Text>
+          </View>
 
-      </View>
-    </ScrollView>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
@@ -175,7 +177,7 @@ const styles = StyleSheet.create({
     fontSize: hp('2%'),
     marginRight: wp('2%'),
     fontWeight: 'bold',
-    color:'black'
+    color: 'black'
   },
   signUpButton: {
     fontSize: hp('2%'),
