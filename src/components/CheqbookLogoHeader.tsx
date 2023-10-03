e46254e479a7412313055e1babbe83bdf11d003e
Felipe Hoyos Taborda <felipe02-98@hotmail.com>
2023-10-03 15:42:57 -0500

    adjust container size

--- src/components/CheqbookLogoHeader.tsx
+++ src/components/CheqbookLogoHeader.tsx
@@ -1,19 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Image, StyleSheet, Dimensions } from 'react-native';
 
 interface LogoHeaderProps {
   logoWidthPercentage: number;
+  style?: object;
 }
 
-const CheqbookLogoHeader: React.FC<LogoHeaderProps> = ({ logoWidthPercentage }) => {
+const logo = require('../../assets/cheqbook-logo.png');
+
+
+const CheqbookLogoHeader: React.FC<LogoHeaderProps> = ({ logoWidthPercentage, style }) => {
   const screenWidth = Dimensions.get('window').width;
   const logoWidth = (screenWidth * logoWidthPercentage) / 100;
 
+  const [logoHeight, setLogoHeight] = useState(0);
+
+  useEffect(() => {
+    Image.getSize(Image.resolveAssetSource(logo).uri, (width, height) => {
+      const calculatedHeight = (logoWidth * height) / width;
+      setLogoHeight(calculatedHeight);
+    });
+  }, []);
+
   return (
-    <View style={styles.container} testID='cheqbook-logo'>
+    <View style={[styles.container, style]}>
       <Image
-        source={require('../../assets/cheqbook-logo.png')}
-        style={{ width: logoWidth, resizeMode: 'contain' }}
+        source={logo}
+        style={{ width: logoWidth, height: logoHeight, resizeMode: 'contain'}}
       />
     </View>
   );
